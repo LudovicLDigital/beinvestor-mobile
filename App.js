b07486709bf6d3ac0d347e276671a970cb78d850
lulauriche <lulauriche>
2020-06-12 16:09:25 +0200

    :sparkles:<feat> (splashscreen) add the splashscreen for android !

--- App.js
+++ App.js
@@ -11,26 +11,29 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import {AppNavigator} from "./app/shared/util/navigation";
 import * as eva from '@eva-design/eva';
 import admob, { MaxAdContentRating } from '@react-native-firebase/admob';
+import SplashScreen from 'react-native-splash-screen';
 
 
 export default class App extends Component {
 
     componentDidMount(): void {
+        SplashScreen.hide();
         admob()
             .setRequestConfiguration({
                 // Update all future requests suitable for parental guidance
-                maxAdContentRating: MaxAdContentRating.PG,
+                maxAdContentRating: MaxAdContentRating.G,
 
                 // Indicates that you want your content treated as child-directed for purposes of COPPA.
                 tagForChildDirectedTreatment: true,
 
                 // Indicates that you want the ad request to be handled in a
                 // manner suitable for users under the age of consent.
-                tagForUnderAgeOfConsent: true,
+                tagForUnderAgeOfConsent: false,
             })
             .then(() => {
                 // Request config successfully set!
             });
+
     }
 
     render() {
