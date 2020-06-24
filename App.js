1fd8bae83d049b9f014525d61f5576c4b7456d1d
lulauriche <lulauriche>
2020-06-24 10:38:51 +0200

    :ok_hand:<fix> (admob) change parental guidance

--- App.js
+++ App.js
@@ -21,14 +21,14 @@ export default class App extends Component {
         admob()
             .setRequestConfiguration({
                 // Update all future requests suitable for parental guidance
-                maxAdContentRating: MaxAdContentRating.G,
+                maxAdContentRating: MaxAdContentRating.PG,
 
                 // Indicates that you want your content treated as child-directed for purposes of COPPA.
                 tagForChildDirectedTreatment: true,
 
                 // Indicates that you want the ad request to be handled in a
                 // manner suitable for users under the age of consent.
-                tagForUnderAgeOfConsent: false,
+                tagForUnderAgeOfConsent: true,
             })
             .then(() => {
                 // Request config successfully set!
