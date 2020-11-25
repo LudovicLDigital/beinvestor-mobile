ad15a760586bc340dece7729928400cd60f4ba5e
Ludovic laurichesse <ludovic.laurichesse@outlook.fr>
2020-11-25 19:01:59 +0100

    :sparkles:<feat>(UserInvestorProfilScreen)integrate Formik bases for the form

--- app/screens/user-profil/user-profil-investor-screen.js
+++ app/screens/user-profil/user-profil-investor-screen.js
@@ -1,25 +1,62 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import {styles} from "../../shared/styles/global";
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, TextInput, View} from 'react-native';
 import {Layout, Text} from '@ui-kitten/components';
 import HeaderBar from '../../component/subcomponent/header-bar';
+import UserInvestorProfil from "../../shared/models/user-investor-profil";
+import UserInvestorProfilService from "../../shared/services/entities/user-investor-profil-service";
+import SectionDivider from "../../component/subcomponent/form/section-divider";
+import { Formik, Form, Field, ErrorMessage, useFormik  } from 'formik';
+import InputField from "../../component/subcomponent/form/input-field";
+import {Button, Input} from "@ui-kitten/components/ui/index";
 
-export default class UserProfilInvestorScreen extends Component {
-    constructor(props) {
-        super(props);
-    }
+export default function UserProfilInvestorScreen(props) {
+    const [userInvestorProfil, setUserInvestorProfil] = useState(new UserInvestorProfil(null,0,0,0,1,0));
+    const userInvestorProfilService = new UserInvestorProfilService();
+    const formik = {
+        initialValues: {
+            salary: userInvestorProfil.professionnalSalary,
+            fiscalPart: userInvestorProfil.fiscalPart,
+            nbEstate: userInvestorProfil.nbEstate,
+            annualRent: userInvestorProfil.annualRent,
+            actualCreditMensualities: userInvestorProfil.actualCreditMensualities
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    };
+    useEffect(() => {
+        if (userInvestorProfil.id === null) {
+            userInvestorProfilService.getCurrentUserInvestorProfil().then((userInvestorProfilRes) => {
+                console.log(userInvestorProfilRes);
+                if (userInvestorProfilRes && userInvestorProfilRes.id !== null) {
+                    setUserInvestorProfil(userInvestorProfilRes)
+                }
+            });
+        }
+    }, [userInvestorProfil]);
 
-    componentDidMount(): void {
-    }
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <HeaderBar route={props.route.name} navigation={props.navigation}/>
+            <Formik
+                initialValues={formik.initialValues}
+                onSubmit={values => formik.onSubmit(values)} >
+                {({ handleChange, handleBlur, handleSubmit, values }) => (
+                    <View style={{flex: 1}}>
+                        <SectionDivider sectionName={'Profil investisseur'}/>
+                        <TextInput
+                            onChange={handleChange("salary")}
+                            onBlur={handleBlur("salary")}
+                            value={values.salary}
+                        />
+                        <SectionDivider sectionName={'Bien immobiliers'}/>
+                        <SectionDivider sectionName={'Détails des biens'}/>
 
-    render() {
-        return (
-            <SafeAreaView style={{ flex: 1 }}>
-                <HeaderBar route={this.props.route.name} navigation={this.props.navigation}/>
-                <Layout style={styles.fullScreen}>
-                    <Text>USER PROFIL</Text>
-                </Layout>
-            </SafeAreaView>
-        );
-    }
+                        <Button onPress={handleSubmit}>Submit</Button>
+                    </View>
+                )}
+            </Formik>
+        </SafeAreaView>
+    );
 }
---
+++ app/shared/services/entities/user-investor-profil-service.js
@@ -0,0 +1,18 @@
+import BaseService from "./base-service";
+
+export default class UserInvestorProfilService extends BaseService {
+    constructor() {
+        super();
+        this.resourceURL = this.resourceURL + '/user-investor-profil';
+    }
+
+    async getCurrentUserInvestorProfil() {
+        return await this.basicGetQuery('/current')
+    }
+    async getUserInvestorProfilByPersonalInfoId(userPersonalInfoId) {
+        return await this.getOneById(null, userPersonalInfoId)
+    }
+    async updateCurrentInvestorProfil(userInvestorDatas) {
+        return await this.updateObject(userInvestorDatas, '/current')
+    }
+}
